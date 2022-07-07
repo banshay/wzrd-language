1085b13a683343e2ca8ccf4d3eac6f5daffa40a4
banshay <importantbopp@gmail.com>
2022-07-07 23:26:17 +0200

    Update buildscripts

--- native/make_native.sh
+++ native/make_native.sh
@@ -40,8 +40,8 @@
 # SOFTWARE.
 #
 
-if [[ $SL_BUILD_NATIVE == "false" ]]; then
-    echo "Skipping the native image build because SL_BUILD_NATIVE is set to false."
+if [[ $BUILD_NATIVE == "false" ]]; then
+    echo "Skipping the native image build because $BUILD_NATIVE is set to false."
     exit 0
 fi
 "$JAVA_HOME"/bin/native-image \
