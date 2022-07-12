2e2aaf25dc0c9e509081580e8d75ca2798d8f39d
banshay <importantbopp@gmail.com>
2022-07-13 01:40:24 +0200

    Commit missing files

---
+++ language/src/main/java/com/banshay/language/nodes/expressions/VariableNode.java
@@ -0,0 +1,24 @@
+package com.banshay.language.nodes.expressions;
+
+import com.banshay.language.WzrdContext;
+import com.banshay.language.nodes.toplevel.WzrdExpressionNode;
+import com.oracle.truffle.api.CompilerDirectives.TruffleBoundary;
+import com.oracle.truffle.api.dsl.NodeField;
+import com.oracle.truffle.api.dsl.Specialization;
+
+@NodeField(name = "name", type = String.class)
+public abstract class VariableNode extends WzrdExpressionNode {
+  protected abstract String getName();
+
+  @Specialization
+  @TruffleBoundary
+  protected Object readVariable(){
+    var context = WzrdContext.get(null);
+    var value = context.globalScope.getVariable(this.getName());
+    if (value == null) {
+      throw new RuntimeException("Variable %s is not defined".formatted(this.getName()));
+    }
+    return value;
+  }
+
+}
